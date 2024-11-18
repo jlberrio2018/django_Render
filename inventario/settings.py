d07ca9599df9ae24c57d8bbb10e835cfed7ceccc
JL BERRIO <jlberrio2018@gmail.com>
2024-11-18 11:17:37 -0500

    configuracion para RENDER

--- inventario/settings.py
+++ inventario/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o!-4rsz1cb4^(0*wf4z@&f#yo#2#1kgiihm@$hft#7p-^mg%0*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Agrega Whitenoise aquí
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
